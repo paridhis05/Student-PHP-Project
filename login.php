41b0e1de437213ced83ab91752c541486ecac749
Paridhi Saini <paridhisaini19@gmail.com>
2025-07-04 17:44:43 +0530

    Create Login form

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="stylelogin.css">
+</head>
+<body>
+
+<div class="center">
+    <h1>Login</h1>
+
+    <div class="form">
+        <input type="text" class="textfield" placeholder="Username">
+        <input type="password" class="textfield" placeholder="Password">
+
+        <div class="forgetpass">
+            <a href="#" class="link">Forgot Password?</a>
+        </div>
+
+        <input type="submit" value="Login" class="btn">
+
+        <div class="signup">
+            New Member? <a href="#" class="link">Sign Up Here</a>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
